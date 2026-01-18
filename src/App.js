ab0d6e01e70841460e81a271fbd672b850bbbc53
KethraPackiamB <kethrapackiamb@gmail.com>
2026-01-18 13:03:39 +0530

    Created the Student Mark List

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navigations from './Browser-Router/Navigations';
 import RoutingPages from './UseParams/RoutingPages';
+import Navigation from "./StudentMarkList/Navigation";
 
 function App() {
   return (
     <div>
         {/* <Navigations/> */}
-        <RoutingPages/>
+        {/* <RoutingPages/> */}
+        <Navigation/>
     </div>
   );
 }
